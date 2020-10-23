3e1d8817462d6bf8bdbd1290fd2ae0c73ca32826
dlugi57 <dlugoszpeter@gmail.com>
2020-10-23 22:58:40 +0200

    DEV user service init

---
+++ src/main/java/com/payMyBuddy/buddy/service/UserService.java
@@ -0,0 +1,9 @@
+package com.payMyBuddy.buddy.service;
+
+import com.payMyBuddy.buddy.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getUsers();
+}
---
+++ src/main/java/com/payMyBuddy/buddy/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.payMyBuddy.buddy.service;
+
+import com.payMyBuddy.buddy.dao.UserDao;
+import com.payMyBuddy.buddy.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * User data manipulation
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    // initialize objects
+    UserDao userDao;
+
+    /**
+     * Field injection of user dao
+     *
+     * @param userDao user dao
+     */
+    @Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    /**
+     * Get all users
+     *
+     * @return List of all users
+     */
+    @Override
+    public List<User> getUsers() {
+        return userDao.findAll();
+    }
+
+
+
+}
