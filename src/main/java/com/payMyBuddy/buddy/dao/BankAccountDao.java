716b1c7f6ba25e9a24e8f3ca0129fc88a7202a64
dlugi57 <dlugoszpeter@gmail.com>
2020-10-26 23:30:20 +0100

    DEV initialize DAO

--- src/main/java/com/payMyBuddy/buddy/dao/BankAccountDao.java
+++ src/main/java/com/payMyBuddy/buddy/dao/BankAccountDao.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BankAccountDao extends JpaRepository<BankAccount, Integer> {
 
+    boolean existsByNumber(String number);
 }
