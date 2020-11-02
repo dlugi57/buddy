2ddc92f626374897faa79a524aa9d1706df73105
dlugi57 <dlugoszpeter@gmail.com>
2020-11-03 00:26:13 +0100

    TEST configuration

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- Schema
+-- -----------------------------------------------------
+CREATE DATABASE  IF NOT EXISTS `paymybuddy` DEFAULT CHARACTER SET utf8;
+USE `paymybuddy`;
